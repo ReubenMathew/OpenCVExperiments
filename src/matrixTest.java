eed81d58d4e9f0135e4f8a4614bb79bacfe2a8d0
Reuben Mathew <intelphysic@gmail.com>
2019-07-21 04:10:07 -0400

    matrix init and exp.

---
+++ src/matrixTest.java
@@ -0,0 +1,20 @@
+import org.opencv.core.*;
+
+public class matrixTest {
+
+	public static void main(String[] args) {
+		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+		
+		Mat matrix = new Mat(5, 5, CvType.CV_8UC1, new Scalar(0));  
+		Mat row0 = matrix.row(0);
+		
+				
+		row0.setTo(new Scalar(1));
+		
+		System.out.println(matrix.dump());
+		
+		
+		
+	}
+
+}
